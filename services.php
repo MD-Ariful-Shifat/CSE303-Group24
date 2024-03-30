7f945c73c0257f3ded71702e10286f3cd694b43f
Salma Tasrian <121690946+salmatasrian85@users.noreply.github.com>
2024-03-30 09:13:24 +0600

    Delete services.php

--- services.php
+++
@@ -1,4 +0,0 @@
-<?php require('components/head.inc.php'); ?>
-<?php require('components/navbar.inc.php'); ?>
-<?php require('components/services.inc.php'); ?>
-<?php require('components/footer.inc.php'); ?>
