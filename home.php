de150fed5a2ff905bdae4c7aea3cd7d6f2cd5825
Salma Tasrian <121690946+salmatasrian85@users.noreply.github.com>
2024-03-30 09:13:15 +0600

    Delete home.php

--- home.php
+++
@@ -1,6 +0,0 @@
-<?php require('components/head.inc.php'); ?>
-<?php require('components/navbar.inc.php'); ?>
-<?php require('components/header.inc.php'); ?>
-<?php require('components/services.inc.php'); ?>
-<?php require('components/icons.inc.php'); ?>
-<?php require('components/footer.inc.php'); ?>
